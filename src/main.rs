3fe1f1d8594eab78d476c7970ed952e7248dbc7e
alwinsDen <jsmadesimple@gmail.com>
2023-03-19 17:10:49 +0530

    Created a freestanding rust binary

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#![no_std]
+#![no_main]
+
+//we will not be using main function as the entrypoint
+use core::panic::PanicInfo;
+
+//this function is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// no_mangle prevents from function name prined as a crytic
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
