e6838f9425e19e57dafb118bfc8d0c83a1cfc91b
alwinsDen <jsmadesimple@gmail.com>
2023-03-20 07:28:50 +0530

    Printing ASCII character by accessing VGA buffer

--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 <h2>To Run the generated .bin in Qemu emulator.</h2>
-<i>qemu-system-x86_64.exe -drive format=raw,file=bootimage-OS-With-Rust.bin</i>
+<i><span>**RUN:** </span>cargo bootimage<i>
+<br>
+<i><span>**RUN:** </span>qemu-system-x86_64.exe -drive format=raw,file=target\llvm_target_descp\debug\bootimage-OS-With-Rust.bin</i>
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 //we will not be using main function as the entrypoint
 use core::panic::PanicInfo;
 
@@ -15,13 +17,14 @@ static HELLO: &[u8] = b"hello world!";
 // no_mangle prevents from function name prined as a crytic
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // let vga_buffer = 0xb8000 as *mut u8;
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
+    vga_buffer::print_something();
     loop {}
 }
 
