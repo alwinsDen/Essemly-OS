24882ce5c3a551d4c74569f15a20970c19c48df0
alwinsDen <jsmadesimple@gmail.com>
2023-03-26 19:58:27 +0530

    feat(testing): Added testing functionality and test cases.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)] //here crate::test_runner is the name of the function.
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
@@ -16,6 +19,30 @@ fn panic(info: &PanicInfo) -> ! {
 
 static HELLO: &[u8] = b"hello world!";
 
+#[cfg(test)] //panic = "abort" needs to be commented in toml for 'cargo test' to run.
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+//here are the added test cases.
+//test case 1
+#[test_case]
+fn trivial_assertion(){
+    print!("trivial assertion...");
+    assert_eq!(1,1);
+    println!("[Ok]");
+}
+
+#[test_case]
+fn test_2(){
+    print!("Test for equality...");
+    assert_eq!(2,2);
+    println!("[Passed]");
+}
+
 // no_mangle prevents from function name prined as a crytic
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -30,10 +57,13 @@ pub extern "C" fn _start() -> ! {
     // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
     // write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}",42,1.337).unwrap();
 
+    //adding test function (only called with 'cargo test')
+    #[cfg(test)]
+    test_main();
+
     //finally using the println macro
     println!("hello world! {}","Alwin");
     panic!("This is a test panic!");
+
     loop {}
 }
-
-//test commit
